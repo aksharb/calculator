56a2f3ecdee28773e912d8291dc25bc00b7e549e
Akshar Bisht <bisht.akshar@gmail.com>
2026-09-07 10:27:49 +0530

    Add logic for calling respective functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const numberButtons = document.querySelectorAll(".digit");
+const decimalButton = document.querySelector("#decimal");
 const operationButtons = document.querySelectorAll(".operation")
 const equalsButton = document.querySelector("#equals");
 const deleteButton = document.querySelector("#delete");
@@ -6,6 +7,18 @@ const clearAllButton = document.querySelector("#clear-all");
 
 const previousOperandTextElement = document.querySelector(".previous-operand");
 const currentOperandTextElement = document.querySelector(".current-operand");
+
+let btnPress = document.querySelectorAll("button");
+
+let firstNumber = "";
+let secondNumber = "";
+
+let firstNumberFormatted;
+let secondNumberFormatted;
+
+let getSecondNumber = false;
+
+let operand = "";
 
 function add(a,b) {
     console.log(a+b);
@@ -24,5 +37,34 @@ function operate(a,b, operand) {
     if(operand ==="+") add(a,b);
     if(operand ==="-") subtract(a,b);
     if(operand ==="*") multiply(a,b);
-    if(operand ==="/") divide(a,b);
+    if(operand ==="÷") divide(a,b);
 }
+
+let setNumber = (e) => {
+    if(!getSecondNumber) {
+    firstNumber += e.target.textContent;
+    firstNumberFormatted = Number(firstNumber);
+    console.log(firstNumberFormatted);
+    }
+    if(getSecondNumber) {
+    secondNumber += e.target.textContent;
+    secondNumberFormatted = Number(secondNumber);
+    console.log(secondNumberFormatted);
+    }
+}
+
+btnPress.forEach((e) => {
+    e.addEventListener("click", function(event) {
+        if(event.target.classList.contains("digit")) {
+            setNumber(event);
+        } 
+        if(event.target.classList.contains("operation")) {
+            getSecondNumber = true;
+            operand = event.target.textContent;
+        }
+        if(event.target.id=="equals") {
+            operate(firstNumberFormatted, secondNumberFormatted, operand);
+        }
+    })
+})
+
