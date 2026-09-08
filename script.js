d8fa7db2b7803cfb01b1820d1ccb15492836a1e4
Akshar Bisht <bisht.akshar@gmail.com>
2026-09-08 06:34:50 +0530

    Add logic to show calculations

--- script.js
+++ script.js
@@ -15,29 +15,38 @@ let secondNumber = "";
 
 let firstNumberFormatted;
 let secondNumberFormatted;
-
+let getFirstNumber = false;
 let getSecondNumber = false;
-
-let operand = "";
+let previous;
+let current;
+let operator = "";
 
 function add(a,b) {
+    return a+b;
     console.log(a+b);
 }
 function subtract(a,b) {
+    return a - b;
     console.log(a-b);
 }
 function multiply(a,b) {
+    return a * b;
     console.log(a*b);
 }
 function divide(a,b) {
-    console.log(a/b);
+    if(b === 0) {
+         return "Can't divide by 0!";
+    } else {
+        return a / b;
+        console.log(a/b);
+    }
 }
 
-function operate(a,b, operand) {
-    if(operand ==="+") add(a,b);
-    if(operand ==="-") subtract(a,b);
-    if(operand ==="*") multiply(a,b);
-    if(operand ==="÷") divide(a,b);
+function operate(a,b, operator) {
+    if(operator ==="+") return(add(a,b));
+    if(operator ==="-") return(subtract(a,b));
+    if(operator ==="*") return(multiply(a,b));
+    if(operator ==="÷") return(divide(a,b));
 }
 
 let setNumber = (e) => {
@@ -45,10 +54,14 @@ let setNumber = (e) => {
     firstNumber += e.target.textContent;
     firstNumberFormatted = Number(firstNumber);
     console.log(firstNumberFormatted);
+    currentOperandTextElement.textContent = firstNumberFormatted;
+    getFirstNumber = true;
     }
-    if(getSecondNumber) {
+    if(getSecondNumber && getFirstNumber) {
     secondNumber += e.target.textContent;
     secondNumberFormatted = Number(secondNumber);
+    previousOperandTextElement.textContent = firstNumberFormatted;
+    currentOperandTextElement.textContent = secondNumber;
     console.log(secondNumberFormatted);
     }
 }
@@ -60,10 +73,30 @@ btnPress.forEach((e) => {
         } 
         if(event.target.classList.contains("operation")) {
             getSecondNumber = true;
-            operand = event.target.textContent;
+            operator = event.target.textContent;
+            previous = firstNumberFormatted;
+            current = secondNumberFormatted;
+            previousOperandTextElement.textContent = currentOperandTextElement.textContent;
+            currentOperandTextElement.textContent = operator;
         }
         if(event.target.id=="equals") {
-            operate(firstNumberFormatted, secondNumberFormatted, operand);
+            if(!getFirstNumber) {
+                window.alert("Please enter a number first");  
+            } else {
+                previousOperandTextElement.textContent = previousOperandTextElement.textContent + operator + currentOperandTextElement.textContent + "=";
+                currentOperandTextElement.textContent = operate(firstNumberFormatted, secondNumberFormatted, operator);
+            }
+        }
+        if(event.target.id=="clear-all") {
+            currentOperandTextElement.textContent = "";
+            previousOperandTextElement.textContent = "";
+            firstNumber = "";
+            secondNumber = "";
+            firstNumberFormatted = 0;
+            secondNumberFormatted = 0;
+            operator = "";
+            getSecondNumber = false;
+            getFirstNumber = false;
         }
     })
 })
